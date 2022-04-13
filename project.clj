bb5ae5e84a7ae3e34182eee6443fa7ab68d24cf1
tom <tom@that-place>
2022-04-12 23:29:45 -0500

    Initial proof of principle.

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject m4server "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [nightlight "2.4.4" #_"2.4.5-SNAPSHOT"
+                  :exclusions
+                  [org.clojure/tools.cli
+                   org.clojure/java.classpath
+                   org.clojure/tools.reader
+                   org.clojure/core.async
+                   org.clojure/tools.analyzer.jvm
+                   org.clojure/tools.analyzer
+                   org.clojure/core.memoize
+                   org.clojure/core.cache
+                   org.clojure/data.priority-map
+                   commons-codec]]
+                 [marathon "4.2.3-SNAPSHOT"]]
+  :plugins [[nightlight/lein-nightlight "2.4.4"]])
---
+++ src/m4server/core.clj
@@ -0,0 +1,6 @@
+(ns m4server.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
